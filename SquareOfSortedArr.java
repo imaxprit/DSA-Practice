9c4045f86da24f6219e57220bac7cbbeec099461
arpit gupta <rajarpitgupta@gmail.com>
2026-07-02 17:56:34 +0530

    SquareOfSortedArr

---
+++ SquareOfSortedArr.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SquareOfSortedArr {
+    public static int[] sortedSquares(int nums[]) {
+        int result[] = new int[nums.length];
+
+        for(int i=0; i<nums.length; i++) {
+            result[i] = nums[i] * nums[i];
+        }
+
+        Arrays.sort(result);
+        return result;
+    }
+
+    public static void main(String args[]) {
+
+        int nums[] = {-4, -1, 0, 3, 10};
+        int output[] = sortedSquares(nums);
+
+        System.out.println(Arrays.toString(output));
+
+    }
+}
