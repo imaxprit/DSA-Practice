a0a8eea4be4291668fd9dddd6aee232f846123c6
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-28 07:43:00 +0530

    Traverse In Array

---
+++ DSA_DAY_01/TraverseArr.java
@@ -0,0 +1,27 @@
+package DSA_DAY_01;
+
+import java.util.Scanner;
+
+public class TraverseArr {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter " + n + " elements: ");
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Traversing the array: ");
+        for(int i=0; i<n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
