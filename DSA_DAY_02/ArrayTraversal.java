8aff7997c7d42f411843c652436269e827ade101
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-29 07:46:04 +0530

    Array Traversal

---
+++ DSA_DAY_02/ArrayTraversal.java
@@ -0,0 +1,12 @@
+package DSA_DAY_02;
+
+public class ArrayTraversal {
+    public static void main(String args[]) {
+
+        int score[] = {74, 85, 96, 69, 58, 47};
+
+        for (int num : score) {
+            System.out.print(num + " ");
+        }
+    }
+}
