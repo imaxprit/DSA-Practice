0eb564b2ff85195e0afa67213c54d04964c3ab97
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-29 07:26:02 +0530

    Array Representation

---
+++ DSA_DAY_02/ArrayRep.java
@@ -0,0 +1,15 @@
+package DSA_DAY_02;
+
+public class ArrayRep {
+    
+    public static void main(String[] args) {
+        
+        int marks[] = {78, 89, 94, 72, 65, 53};
+
+        System.out.print("Marks Are : ");
+
+        for(int no : marks) {
+            System.out.print(no + " ");
+        }
+    }
+}
