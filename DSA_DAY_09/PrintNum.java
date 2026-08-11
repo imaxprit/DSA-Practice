5ed6715ae726df2841943d0f54dcdd7d97b2a871
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-08-11 07:25:14 +0530

    Program to Print Num from 1 to N

---
+++ DSA_DAY_09/PrintNum.java
@@ -0,0 +1,16 @@
+package DSA_DAY_09;
+
+public class PrintNum {
+    
+    public static void main(String[] args) {
+
+        // WAP to print numbers from 1 to n
+        
+        int n = 10;
+        int i = 1;
+        while (i <= n) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
