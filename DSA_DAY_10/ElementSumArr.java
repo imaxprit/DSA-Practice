d3cc450193cc221905cf63bc12e809aa3a272440
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-08-12 07:41:59 +0530

    Sum and Average of array Elements

--- DSA_DAY_10/ElementSumArr.java
+++ DSA_DAY_10/ElementSumArr.java
@@ -4,13 +4,16 @@ public class ElementSumArr {
     
     public static void main(String args[]) {
 
-        int nums[] = {7, 4, 1, 8, 5, 2, 9, 6, 3};
+        int nums[] = {7, 4, 1, 8, 5, 2, 9, 6};
         int sum = 0;
 
         for(int i=0; i<nums.length; i++) {
             sum += nums[i];
         }
 
+        float average = sum / nums.length;
+
         System.out.println("Sum of Element in Array is "+sum);
+        System.out.println("Average of Elements in Array is " + average);
     }
 }
