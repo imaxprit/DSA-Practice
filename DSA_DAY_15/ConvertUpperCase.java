9d7dd9e7b1919c7bf6407867dd0389963ef7b943
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-01 08:10:27 +0530

    Capitalize first Character of String

---
+++ DSA_DAY_15/ConvertUpperCase.java
@@ -0,0 +1,30 @@
+package DSA_DAY_15;
+
+public class ConvertUpperCase {
+
+    public static String toUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1; i<str.length(); i++) {
+            if(str.charAt(i)==' ' && i<str.length()-1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        } 
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) {
+        
+        String str = "hello, i am arpit gupta.";
+        System.out.println(toUpperCase(str));
+
+        String str1 = "what is your name";
+        System.out.println(toUpperCase(str1));
+    }
+}
