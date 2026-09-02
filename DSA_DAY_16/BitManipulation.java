1aa7c74107a36fd45ce371d1bb49f9c302a76313
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-02 08:28:06 +0530

    Bitwise Operators

---
+++ DSA_DAY_16/BitManipulation.java
@@ -0,0 +1,23 @@
+package DSA_DAY_16;
+
+public class BitManipulation {
+    
+    // Bitwise operator
+    public static void main(String[] args) {
+        
+        // Binary AND &
+        System.out.println(5 & 6);
+
+        // Binary OR |
+        System.out.println(5 | 6);
+
+        // Binary XOR ^
+        System.out.println(5 ^ 6);
+
+        // Binary One's Complement ~ 
+        System.out.println(~5);     // ~n = -(n+1)
+        System.out.println(~9);
+
+        
+    }
+}
