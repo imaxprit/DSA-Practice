0141b86181171dc1821f1d16a0ce40074b92a3d7
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-07 08:12:32 +0530

    Constructors

---
+++ DSA_DAY_18/TestConst.java
@@ -0,0 +1,14 @@
+package DSA_DAY_18;
+
+public class TestConst {
+
+    public TestConst() {
+        System.out.println("I am a constructor");
+    }
+    
+    public static void main(String args[]) {
+
+        TestConst t = new TestConst();
+        System.out.println(t);
+    }
+}
