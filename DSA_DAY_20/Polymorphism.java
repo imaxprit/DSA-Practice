972eca7dafae708857428f64ec9abbc4590a8cab
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-09 10:07:09 +0530

    Method overriding

--- DSA_DAY_20/Polymorphism.java
+++ DSA_DAY_20/Polymorphism.java
@@ -4,10 +4,13 @@ public class Polymorphism {
     
     public static void main(String[] args) {
         
-        Calculator calc = new Calculator();
-        System.out.println(calc.sum(14, 25));
-        System.out.println(calc.sum(2.5f, 7.4f));
-        System.out.println(calc.sum(24, 15, 36));
+        // Calculator calc = new Calculator();
+        // System.out.println(calc.sum(14, 25));
+        // System.out.println(calc.sum(2.5f, 7.4f));
+        // System.out.println(calc.sum(24, 15, 36));
+
+        Dolphine dolph = new Dolphine();
+        dolph.swim();
     }
 }
 
@@ -22,5 +25,17 @@ class Calculator {
 
     int sum(int a, int b, int c) {
         return a + b + c;
+    }
+}
+
+class Fish {
+    void swim() {
+        System.out.println("fishes are under water.");
+    }
+}
+
+class Dolphine extends Fish {
+    void swim() {
+        System.out.println("Dolphine have some generic sound");
     }
 }
