0f6d28e7f302c48be417a8f05803e755ed53da9b
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-14 08:11:06 +0530

    Interface in java

---
+++ DSA_DAY_22/Interface.java
@@ -0,0 +1,24 @@
+package DSA_DAY_22;
+
+public class Interface {
+    
+    public static void main(String args[]) {
+        Vehicle v = new Car();
+        v.start();
+        v.stop();
+    }
+}
+
+interface Vehicle {
+    void start();
+    void stop();
+}
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car is Starting...");
+    } 
+    public void stop() {
+        System.out.println("Car is stopping...");
+    }
+}
