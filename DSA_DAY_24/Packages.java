bfb315ccbcd30ef303d6252bb02703c01824e98b
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-16 08:22:36 +0530

    Access modifiers in java

--- DSA_DAY_24/Packages.java
+++ DSA_DAY_24/Packages.java
@@ -1,4 +1,4 @@
-package DSA_DAY_24;
+package DSA_DAY_24; //User-Defined Packages
 
 import java.util.Scanner;   //Built-in Packages
 
@@ -11,5 +11,20 @@ public class Packages {
         String language = sc.nextLine();
         System.out.println("Thanks to choose " + language + " language.");
         sc.close();
+
+        Car c = new Car();
+        c.startEngine();
     }
+}
+
+// Access Modifiers
+class Car {
+    private String model = "nex10";
+    protected int speed = 40;
+    public void startEngine() {
+        System.out.println("Engine Started");
+        System.out.println(model);
+        System.out.println(speed);
+    }
+    
 }
