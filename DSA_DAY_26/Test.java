d77ffea301ab4d20a31d56537dd24d25927868f5
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-19 10:39:27 +0530

    test case in oop

---
+++ DSA_DAY_26/Test.java
@@ -0,0 +1,28 @@
+package DSA_DAY_26;
+
+
+public class Test {
+    public static void main(String args[]) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+        p1.setColor("Yellow");
+        System.out.println(p1.color);
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
